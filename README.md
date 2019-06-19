7dc3a624c01727fe976356ebcb5dde8b23fba413
Anthony Micallef <anthony.micallef@me.com>
2019-06-19 14:14:04 -0400

    Added prettier formatting

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br>
 
 ### Try it!
+
 Demo available [here](https://disposablethumbs.github.io/react-concentration-game/).
--- src/Card.js
+++ src/Card.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
-import './Card.css';
+import React, { Component } from "react";
+import "./Card.css";
 
 class Card extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  clicked(card) {
+    this.props.click(card);
+  }
+  render() {
+    return (
+      <div
+        className={
+          "card" +
+          (!this.props.close ? " opened" : "") +
+          (this.props.complete ? " matched" : "")
         }
-    }
-    clicked(card) {
-        this.props.click(card)
-    }
-    render() {
-        return (
-            <div className={"card" + (!this.props.close ? ' opened' : '') + (this.props.complete ? ' matched' : '')} onClick={() => this.clicked(this.props.card)}>
-                <div className="front">
-                    ?
-                </div>
-                <div className="back">
-                    <span className={this.props.card}></span>
-                </div>
-            </div>
-        )
-    }
+        onClick={() => this.clicked(this.props.card)}
+      >
+        <div className="front">?</div>
+        <div className="back">
+          <span className={this.props.card} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
