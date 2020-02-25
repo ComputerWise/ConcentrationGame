d836abeaa7a205a701ec15a47242c12c59286d26
Anthony Micallef <anthony.micallef@me.com>
2020-02-25 16:17:56 -0500

    Updates

---
+++ precache-manifest.6fe6ab686d55fa56f517a668663c44ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9aad5f317d377794d55",
+    "url": "/ConcentrationGame/static/js/runtime~main.5d433c5a.js"
+  },
+  {
+    "revision": "7b5d19bd54d21e8eac4f",
+    "url": "/ConcentrationGame/static/js/main.b8b8c9f8.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ConcentrationGame/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7b5d19bd54d21e8eac4f",
+    "url": "/ConcentrationGame/static/css/main.d5a586fa.chunk.css"
+  },
+  {
+    "revision": "93d42d98d58fc9fcba1f1ae884a32098",
+    "url": "/ConcentrationGame/index.html"
+  }
+];
